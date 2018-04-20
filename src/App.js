189a8c79c78ad9ba501f9c1569e5c61af77aa145
sratna <adhikariratna52@gmail.com>
2018-04-20 14:41:05 +0545

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Tic Tac Toe Game
+        <div className="header">
+          <span>Team O</span>
+        </div>
       </div>
     );
   }
